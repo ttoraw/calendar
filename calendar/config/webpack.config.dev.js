3b756f1f8004fbac89088be20fe012abb40b64ed
helen030 <ethel2739@hotmail.com>
2019-11-18 17:56:57 +0900

    settings...

---
+++ calendar/config/webpack.config.dev.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../build")
+  },
+  mode:"development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "../build"),
+    index: "index.html",
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  resolve: {
+   extensions: [".jsx", ".js"]
+ },
+  plugins: [
+    new HtmlWebPackPlugin({
+            template: './public/index.html', // public/index.html 파일을 읽는다.
+      filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
+    }),
+    new MiniCssExtractPlugin({
+     filename: 'style.css'
+     // filename: 'style-test.css'
+   }),
+    new CleanWebpackPlugin()
+  ]
+};
---
+++ calendar/src/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import './style.scss';
+
+const Root = () => {
+  return (
+    <div>
+      <h3 className="title">Hello, React</h3>
+      <h3 className="title_scss">Hello, React</h3>
+    </div>
+  );
+};
+
+export default Root;
