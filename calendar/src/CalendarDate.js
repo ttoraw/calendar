1a8b636be5853cb4d4180f6c6fe69c6a6d0e1f2d
helen030 <ethel2739@hotmail.com>
2019-11-20 21:36:57 +0900

    date span 추가

--- calendar/src/CalendarDate.js
+++ calendar/src/CalendarDate.js
@@ -16,7 +16,13 @@ class CalendarDate extends React.Component {
     let dateNumber = this.state.date<10?"0"+this.state.date : this.state.date
     return(
       <button>
-        <time dateTime={this.state.year+"-"+this.state.month+"-"+dateNumber}>{this.state.date}</time>
+        <span style={{display:'block',position: 'absolute', top: '0px'}}>
+          <time dateTime={this.state.year+"-"+this.state.month+"-"+dateNumber}>{this.state.date}</time>
+        </span>
+        <span style={{backgroundColor:"red", display:'block', margin:'2px'}}>span1</span>
+      	<span style={{backgroundColor:"blue", display:'block', margin:'2px'}}>span2</span>
+      	<span style={{backgroundColor:"green", display:'block', margin:'2px'}}>span3</span>
+        <span style={{backgroundColor:"pink", display:'block', margin:'2px'}}>span4</span>
       </button>
     )
   }
