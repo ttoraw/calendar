ae95b3d69f9303ccdc31fe2208cd4b99276506d2
helen030 <ethel2739@hotmail.com>
2019-11-28 22:24:12 +0900

    server 실행 : nodemon --exec babel-node server\main.js 앱실행 : yarn start

--- calendar/config/webpack.config.dev.js
+++ calendar/config/webpack.config.dev.js
@@ -2,8 +2,17 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+const webpack = require('webpack');
+
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["./src/index.js",
+
+  'webpack-dev-server/client?http://0.0.0.0:4000',
+         'webpack/hot/only-dev-server'
+       ],
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "../build")
@@ -12,7 +21,23 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, "../build"),
     index: "index.html",
-    port: 9000
+
+    hot: true,
+    historyApiFallback: true,
+    //port: 9000,
+    proxy: {
+            "**": "http://localhost:3000"
+        },
+        stats: {
+          // Config for minimal console.log mess.
+          assets: false,
+          colors: true,
+          version: false,
+          hash: false,
+          timings: false,
+          chunks: false,
+          chunkModules: false
+        }
   },
   module: {
     rules: [
@@ -38,7 +63,8 @@ module.exports = {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
-    ]
+    ],
+
   },
   resolve: {
    extensions: [".jsx", ".js"]
@@ -52,6 +78,9 @@ module.exports = {
      filename: 'style.css'
      // filename: 'style-test.css'
    }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+
+    new webpack.HotModuleReplacementPlugin()
+
   ]
 };
